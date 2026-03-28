38744ce82004204fccb9a983e2ab901e842a9b0e
developOnion <smeyonion.nou@gmail.com>
2026-03-28 15:45:30 +0700

    style(author): format AuthorMapper.java for consistency

--- src/main/java/com/oop/library_management/author/AuthorMapper.java
+++ src/main/java/com/oop/library_management/author/AuthorMapper.java
@@ -6,18 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthorMapper extends BaseMapper<Author, AuthorResponseDTO> {
 
-	@Override
-	public AuthorResponseDTO toDTO(Author author) {
-		if (author == null) {
-			return null;
-		}
+  @Override
+  public AuthorResponseDTO toDTO(Author author) {
+    if (author == null) {
+      return null;
+    }
 
-		return new AuthorResponseDTO(
-			author.getId(),
-			author.getFullName(),
-			author.getFirstName(),
-			author.getLastName(),
-			author.getType()
-		);
-	}
+    return new AuthorResponseDTO(
+        author.getId(),
+        author.getFullName(),
+        author.getFirstName(),
+        author.getLastName(),
+        author.getType());
+  }
 }
